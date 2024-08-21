08ee2698d457342b27a154fe24459bb9a7658936
lapaoss <paola.sandoval.s@ucb.edu.bo>
2024-08-21 19:15:40 -0400

    hackerrange3-6

---
+++ codigos/hackerrange suma.py
@@ -0,0 +1,21 @@
+# ejercicio 3
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+# ejercicio 4
+if __name__ == '__main__':
+    n = int(input())
+    for i in  range (n):
+        print (i*i)
+
+
+#ejercicio  5
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (n):
+        print(i+1, end="")
