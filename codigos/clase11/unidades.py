08efaedd022043cad1abdf25e8c5b54d18632f08
lapaoss <paola.sandoval.s@ucb.edu.bo>
2024-09-11 22:20:01 -0400

    conversiones

---
+++ codigos/clase11/unidades.py
@@ -0,0 +1,6 @@
+def longitud(a):
+    return (a*1000)
+def masa(a):
+    return (a*1000)
+def temperatura(a):
+    return (a*9/5+32)
---
+++ codigos/notas.py
@@ -0,0 +1,13 @@
+notas=[]
+n=int(input("numero de notas "))
+for i in range(n):
+    notas.append(int(input(f"Ingrese nota{i+1}=")))
+    
+
+
+
+
+
+
+
+
